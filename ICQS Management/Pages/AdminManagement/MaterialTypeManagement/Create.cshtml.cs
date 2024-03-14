a414578ae0d64b838d3050befb595c1a141e1e74
diemyolo <quydiem2015@gmail.com>
2024-03-14 17:35:09 +0700

    update 1

---
+++ ICQS Management/Pages/AdminManagement/MaterialTypeManagement/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using BusinessObject.Entity;
+using DataAccessLayer.ApplicationDbContext;
+
+namespace ICQS_Management.Pages.AdminManagement.MaterialTypeManagement
+{
+    public class CreateModel : PageModel
+    {
+        private readonly DataAccessLayer.ApplicationDbContext.applicationDbContext _context;
+
+        public CreateModel(DataAccessLayer.ApplicationDbContext.applicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public MaterialType MaterialType { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.MaterialTypes.Add(MaterialType);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ ICQS Management/Pages/AdminManagement/MaterialTypeManagement/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BusinessObject.Entity;
+using DataAccessLayer.ApplicationDbContext;
+
+namespace ICQS_Management.Pages.AdminManagement.MaterialTypeManagement
+{
+    public class EditModel : PageModel
+    {
+        private readonly DataAccessLayer.ApplicationDbContext.applicationDbContext _context;
+
+        public EditModel(DataAccessLayer.ApplicationDbContext.applicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public MaterialType MaterialType { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            MaterialType = await _context.MaterialTypes.FirstOrDefaultAsync(m => m.MaterialTypeId == id);
+
+            if (MaterialType == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(MaterialType).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MaterialTypeExists(MaterialType.MaterialTypeId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool MaterialTypeExists(Guid id)
+        {
+            return _context.MaterialTypes.Any(e => e.MaterialTypeId == id);
+        }
+    }
+}
